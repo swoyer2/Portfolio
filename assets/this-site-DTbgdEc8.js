5ef2975cf3b42c66d748b648c6d2f0dc5a6edeef
Logan Swoyer <43830142+swoyer2@users.noreply.github.com>
2026-03-31 20:34:09 -0400

    Updates

---
+++ assets/this-site-DTbgdEc8.js
@@ -0,0 +1,26 @@
+const e=`# This site\r
+Jan 21 2026\r
+\r
+Welcome to the first blog post I have ever made. I am not much of a writer, but I do occasionally make cool projects.\r
+The only problem is some of these projects end up sitting on my SSD never to get seen by anyone except myself and\r
+my girlfriend, *who could care less about seeing it*. Throughout my time making projects I have stumbled across a couple\r
+of blogs of other people making similar projects to mine, these are always a fun read for me and it lead me to think about\r
+making my own blog. Some of the inspirations: [Sam Schneider](https://selectfromwhereand.com/posts) and \r
+[Anthony Fu](https://selectfromwhereand.com/projects/connections/).\r
+\r
+This site actually is heavily inspired by Anthony Fu's portfolio site, I liked the minimialistic style looked and the way the background\r
+canvas with a growing tree or dot grid looked. As you can see on this site, I have a background grid with a boids algorithm\r
+but instead of making them look like birds, I have the parameters set to make it look more like cells. I thought this looked nice and\r
+was less distracting. **Try hovering over a blob if you haven't already**.\r
+\r
+Hopefully within the next 2 weeks I will have a blog post documenting a project I am working on currently. It is a yahtzee *AI* written in\r
+C that should hopefully by the end of the project play better on average the a human player. I also have some other projects in mind\r
+that I would like to create then show, but that will be revealed in time.\r
+\r
+The last thing I will mention is that I do like to read books about computer science so potentially I will write some thoughts and things I\r
+learned from the books I read. Currently I am reading [Code: The Hidden Language of Computer Hardware and software, 2nd Edition](https://www.microsoftpressstore.com/store/code-the-hidden-language-of-computer-hardware-and-software-9780137909100).\r
+It's a bit of a mouthful but the book is very good so far, but a little too basic for me in the first couple chapters. However I have skimmed \r
+some of the later chapters and it will eventually get to a point where I will be learning more.\r
+\r
+\r
+`;export{e as default};
